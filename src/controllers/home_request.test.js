fb242cfadb05eb757ffd584e8b4a4f2149319eb1
metamiaip <metamia@gmail.com>
2021-11-15 22:42:22 +0000

    "GET request for home endpoint and test

---
+++ src/controllers/home_request.test.js
@@ -0,0 +1,10 @@
+const request = require("supertest");
+const app = require("../app");
+
+describe("Test home API endpoint request", () => {
+  test("GET should return correct message", async () => {
+    const res = await request(app).get("/");
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toEqual("Welcome to the Drinks API!");
+  });
+});
