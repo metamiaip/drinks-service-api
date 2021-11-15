d641d2e1f0fbad722f86e74211cd754b80236da3
metamiaip <metamia@gmail.com>
2021-11-15 23:13:28 +0000

    Write extra tests for the coffee controller

--- src/controllers/coffee_request.test.js
+++ src/controllers/coffee_request.test.js
@@ -12,3 +12,26 @@ test("GET /coffee should return correct object", async () => {
     name: "Latte",
   });
 });
+
+test("GET /coffee without passing coffeeName should return correct object", async () => {
+  const res = await request(app)
+  .get("/coffee")
+  
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: "Coffee",
+    name: "Latte",
+  });
+});
+
+test("GET /coffee with coffeeName not equal to Latte should return correct object", async () => {
+  const res = await request(app)
+  .get("/coffee")
+  .query({ coffeeName: "Expresso" });
+
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: "Coffee",
+    name: "Expresso",
+  });
+});
