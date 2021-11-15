cedc4a6597ddf29420f2f749c3747f5dba06943b
metamiaip <metamia@gmail.com>
2021-11-15 22:48:44 +0000

    Create the Coffee Controller and the GET Endpoint for /coffeelover

--- src/app.js
+++ src/app.js
@@ -3,4 +3,6 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Welcome to the Drinks API!"));
 
+app.get("/coffeelover", (req, res) => res.send("I like coffee!"));
+
 module.exports = app;
--- src/controllers/home_request.test.js
+++ src/controllers/home_request.test.js
@@ -8,3 +8,11 @@ describe("Test home API endpoint request", () => {
     expect(res.text).toEqual("Welcome to the Drinks API!");
   });
 });
+
+describe("Test coffeelover API endpoint request", () => {
+  test("GET should return correct message", async () => {
+    const res = await request(app).get("/coffeelover");
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toEqual("I like coffee!");
+  });
+});
